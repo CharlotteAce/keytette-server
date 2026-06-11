b4aee6554f2383b067fb966120ccbae4681e2d1b
Charlotteace <149445926+CharlotteAce@users.noreply.github.com>
2026-06-11 16:25:51 +0900

    headerの追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { App } from "./pages/TopPage";
 import { Player } from "./components/Player";
 import { AlbumPage } from "./pages/AlbumPage";
+import { Header } from "./components/Header";
 import "normalize.css"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/:category/:albumName" element={<AlbumPage />} />
       </Routes>
       <div className="footer">Powered by <a href="https://github.com/CharlotteAce/keytette" target="_blank" rel="noopener noreferrer">key-tette</a> / <a href="https://charlotteace.github.io" target="_blank" rel="noopener noreferrer">charlotteace</a></div>
       <Player />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
