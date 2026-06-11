8eb5ab4cc0fff40b60b9f88df8f0994e58c6a2dc
Charlotteace <149445926+CharlotteAce@users.noreply.github.com>
2026-06-11 16:52:27 +0900

    headerの詳細を追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export function Header() {
   return (
-    <header>
-    </header>
+    <>
+      <header>
+        <div id="header-wrapper">
+          <div id="title">
+            <Link to="/">key-tette</Link>
+          </div>
+          <div id="subtitle"><a href="https://charlotteace.jp/" target="_blank" rel="noopener noreferrer">on シャーロットエースの部屋</a></div>
+        </div>
+      </header>
+      <div id="header-wrapper">
+        <p><small>本ページは、即売会向け試聴サーバ「key-tette」の動作デモサイトです。</small></p>
+      </div>
+      
+    </>
   );
 }
